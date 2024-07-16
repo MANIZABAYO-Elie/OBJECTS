45d1eb2a4eeedcca6bd2d8d54f8ccda3352274b4
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-07-16 13:06:45 +0200

    file commited

--- objects.js
+++ objects.js
@@ -1,8 +1,12 @@
 const person1 ={
 firstName :"JAMES",
 lastName  : "Harden",
-age  : 30
+age  : 30,
 
 
 }
+const fullNames= (firstName,lastName)=>{
+    return firstName + lastName
+    }
 console.log(person1.firstName);
+console.log(fullNames);
