bf69288b2d72c79d95e667825e24d431b8fc0fc2
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-07-16 23:06:21 +0200

    file commited

--- objects.js
+++ objects.js
@@ -11,3 +11,13 @@ const fullNames= (firstName,lastName)=>{
 console.log(person1.firstName);
 console.log(person1.lastName);
 console.log(fullNames);
+// this keyword
+const person2 ={
+    firstName :"Jane",
+    lastName  : "SMITH",
+    age  : 20,
+    
+    
+    }
+    console.log(`${this.age}`);
+    console.log(`${this.firstName} is cleaning the room`);
